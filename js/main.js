72baa933a4631f27719010b47b892899e9be87b1
monomagdy <39876151+monomagdy@users.noreply.github.com>
2018-11-20 04:43:53 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*Logo image animation*/
+$(".logo-section img").on({
+  "mouseover": function() {
+    this.src = "img/logo-mona-eye.png";
+  },
+  "mouseout": function() {
+    this.src = "img/logo-mona.png";
+  }
+});
